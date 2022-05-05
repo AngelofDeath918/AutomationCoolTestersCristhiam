d656c0f108a2d95148b2c3d88ce3890f97fdc9ba
Cristhiam <cdg.918@hotmail.com>
2022-05-05 18:25:45 -0500

    Multiplicacion ejercicio

---
+++ AutomationCoolTestersCristhiam/src/test/java/com/java/Multiplicacion.java
@@ -0,0 +1,14 @@
+package com.java;
+
+public class Multiplicacion {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int a= 2, b=3, c=4;
+		
+		System.out.println(a*b*c);
+
+	}
+
+}
